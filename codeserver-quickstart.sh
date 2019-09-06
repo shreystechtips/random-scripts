87140f9ffa8b968467edb187b9422c74ce37bcc1
Shrey Aeron <shreystechtips@gmail.com>
2019-09-06 10:40:37 -0700

    Update codeserver-quickstart.sh

--- codeserver-quickstart.sh
+++ codeserver-quickstart.sh
@@ -1,5 +1,5 @@
 echo "download start"
-wget code.tar.gz "https://github.com/cdr/code-server/releases/download/2.preview.11-vsc1.37.0/code-server2.preview.11-vsc1.37.0-linux-x86_64.tar.gz"
+wget "https://github.com/cdr/code-server/releases/download/2.preview.11-vsc1.37.0/code-server2.preview.11-vsc1.37.0-linux-x86_64.tar.gz" -o code.tar.gz
 echo "downloaded"
 tar -xf code.tar.gz
 echo "extracted"
